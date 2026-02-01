5398bfb90a6ad685ae2dd2f888438d2ffdd0801f
Muhammad Firdaus <mfirdaus1453@gmail.com>
2026-02-01 09:00:40 +0700

    fix: restore InitDB function signature

--- database/database.go
+++ database/database.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
+func InitDB(connectionString string) (*sql.DB, error) {
     // Fallback: Switch to port 5432 and force SSL if 6543 fails
     connectionString = strings.Replace(connectionString, ":6543", ":5432", 1)
 
